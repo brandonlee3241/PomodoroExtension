778e010b60658e3c4564f65312f34d5b92c375bb
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 <bjl12@sfu.ca>
2023-09-25 15:55:39 -0700

    basic countdown functionality implemented successfully

--- background.js
+++ background.js
@@ -1,47 +1,43 @@
-let isExtensionActive = false;
 
-chrome.runtime.onInstalled.addListener(() => {
-    // Initialize the extension state in storage
-    chrome.storage.local.set({ isExtensionActive: false });
 
-    // Set the initial badge text to "OFF"
-    chrome.action.setBadgeText({
-        text: "OFF",
-    });
-});
 
-// Create a listener so that the extension can be toggled on and off 
-// and will persist across browser sessions and page reloads
+// start the timer in the background, continuously feeding the popup.js with the time remaining
+let timerId;
+let timeRemaining = 0;
 
-// The listener will be called when the extension icon is clicked
-chrome.action.onClicked.addListener(() => {
-    isExtensionActive = !isExtensionActive;
-
-    chrome.storage.local.set({ isExtensionActive });
-
-    // Update the badge text to reflect the extension state
-    chrome.action.setBadgeText({
-        text: isExtensionActive ? "ON" : "OFF",
-    });
-});
-
-// Create a listener for when the extension is active and the user navigates to a new page
-// The listener will be called when the tab is updated
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // Check if the extension is active
-    chrome.storage.local.get("isExtensionActive", (data) => {
-        if (data.isExtensionActive) {
-            // If the extension is active, inject the CSS and JS into the page
-            chrome.scripting.insertCSS({
-                target: { tabId },
-                files: ["focus-mode.css"],
-            });
+function startTimer(timeRemaining){
+    timerId = setInterval(function(){
+        timeRemaining--;
+        console.log("time remaining: " + timeRemaining + " seconds");
+        sendTimeRemaining(timeRemaining);
+        if(timeRemaining === 0){
+            console.log("timer is done");
+            stopTimer();
         }
-        else {
-            chrome.scripting.removeCSS({
-                target: { tabId },
-                files: ["focus-mode.css"],
-            });
-        }
-    });
+    }, 1000);
+};
+
+function stopTimer(){
+    clearInterval(timerId);
+    timeRemaining = 0;
+    sendTimeRemaining(timeRemaining);
+};
+// sends the time remaining to popup.js
+function sendTimeRemaining(timeRemaining){
+    const minutes = Math.floor(timeRemaining / 60);
+    const seconds = timeRemaining % 60;
+    chrome.runtime.sendMessage({action: "updateTimer", time: `${minutes}:${seconds.toString().padStart(2, "0")}`});
+};
+
+
+// listen for message from popup.js to start
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+// listens for action to start or stop timer
+    if(request.action === "startTimer"){
+        // start timer
+        startTimer(request.duration);
+    }
+    else if(request.action === "stop"){
+        stopTimer();
+    }
 });
---
+++ popup.js
@@ -0,0 +1,23 @@
+// DOM loader
+document.addEventListener("DOMContentLoaded", function () {
+
+    // set the duration to 25 minutes
+    document.getElementById("timer").value = 5;
+
+    // Listen for start button click
+    document.getElementById("start").addEventListener("click", function () {
+        // tell background.js it was clicked
+
+        let duration = document.getElementById("timer").value;
+        chrome.runtime.sendMessage({ duration: duration, action: "startTimer" });
+    })
+
+    // Listen for updateTimer message from background.js
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request.action === "updateTimer") {
+            document.getElementById("timer").textContent = request.time;
+        }
+    });
+});
+
+
